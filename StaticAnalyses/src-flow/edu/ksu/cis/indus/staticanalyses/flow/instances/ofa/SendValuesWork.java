72f7cee1df5462d70c455119da7e24f3cadab531
venku <>
2003-08-18 08:34:20 +0000

    Well, used the object pool as available from jakarta commons implementation.

--- StaticAnalyses/src-flow/edu/ksu/cis/indus/staticanalyses/flow/instances/ofa/SendValuesWork.java
+++ StaticAnalyses/src-flow/edu/ksu/cis/indus/staticanalyses/flow/instances/ofa/SendValuesWork.java
@@ -38,9 +38,13 @@ package edu.ksu.cis.indus.staticanalyses.flow.instances.ofa;
 import edu.ksu.cis.indus.staticanalyses.flow.AbstractWork;
 import edu.ksu.cis.indus.staticanalyses.flow.IFGNode;
 
-import java.util.ArrayList;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.pool.BasePoolableObjectFactory;
+import org.apache.commons.pool.ObjectPool;
+import org.apache.commons.pool.impl.SoftReferenceObjectPool;
+
 import java.util.Collection;
-import java.util.List;
 
 
 /**
@@ -54,8 +58,23 @@ public class SendValuesWork
   extends AbstractWork {
 	/**
 	 * This is the work pool of work peices that will be reused upon request..
+	 *
+	 * @invariant POOL.borrowObject().oclIsKindOf(SendValuesWork)
 	 */
-	private static final List POOL = new ArrayList();
+	private static final ObjectPool POOL =
+		new SoftReferenceObjectPool(new BasePoolableObjectFactory() {
+				/**
+				 * @see org.apache.commons.pool.PoolableObjectFactory#makeObject()
+				 */
+				public Object makeObject() {
+					return new SendValuesWork();
+				}
+			});
+
+	/**
+	 * The logger used by instances of this class to log messages.
+	 */
+	private static final Log LOGGER = LogFactory.getLog(SendValuesWork.class);
 
 	/**
 	 * Injects the values into the associated node.
@@ -66,9 +85,18 @@ public class SendValuesWork
 
 	/**
 	 * Puts back this work into the work pool to be reused.
+	 *
+	 * @throws RuntimeException if the return of the object to the pool failed.
 	 */
 	protected void processed() {
-		POOL.add(this);
+		try {
+			POOL.returnObject(this);
+		} catch (Exception e) {
+			if (LOGGER.isWarnEnabled()) {
+				LOGGER.warn("How can this happen?", e);
+			}
+			throw new RuntimeException(e);
+		}
 	}
 
 	/**
@@ -83,13 +111,8 @@ public class SendValuesWork
 	 * @pre toNode != null and valueToBeSent != null
 	 */
 	static final SendValuesWork getWork(final IFGNode toNode, final Object valueToBeSent) {
-		SendValuesWork result;
+		SendValuesWork result = getWork();
 
-		if (POOL.isEmpty()) {
-			result = new SendValuesWork();
-		} else {
-			result = (SendValuesWork) POOL.remove(0);
-		}
 		result.setFGNode(toNode);
 		result.addValue(valueToBeSent);
 		return result;
@@ -107,30 +130,48 @@ public class SendValuesWork
 	 * @pre toNode != null and valuesToBeSent != null
 	 */
 	static final SendValuesWork getWork(final IFGNode toNode, final Collection valuesToBeSent) {
-		SendValuesWork result;
-
-		if (POOL.isEmpty()) {
-			result = new SendValuesWork();
-		} else {
-			result = (SendValuesWork) POOL.remove(0);
-		}
+		SendValuesWork result = getWork();
 		result.setFGNode(toNode);
 		result.addValues(valuesToBeSent);
+		return result;
+	}
+
+	/**
+	 * Returns a work peice from the pool.
+	 *
+	 * @return the work peice
+	 *
+	 * @throws RuntimeException if the work peice could not be retrieved.
+	 */
+	private static SendValuesWork getWork() {
+		SendValuesWork result;
+
+		try {
+			result = (SendValuesWork) POOL.borrowObject();
+		} catch (Exception e) {
+			if (LOGGER.isWarnEnabled()) {
+				LOGGER.warn("How can this happen?", e);
+			}
+			throw new RuntimeException(e);
+		}
 		return result;
 	}
 }
 
 /*
    ChangeLog:
-   
+
    $Log$
+
+   Revision 1.3  2003/08/18 07:09:13  venku
+   The way objects were removed from the pool was incorrect.  I had used get() instead of remove().
+   This will not remove the object from the pool.  This has been fixed.
+
    Revision 1.2  2003/08/18 01:01:18  venku
    Trying to fix CVS's erratic behavior.
 
-   
    Revision 1.1  2003/08/17 11:19:13  venku
    Placed the simple SendValuesWork class into a separate file.
    Extended it with work pool support.
    Amended AbstractWork and WorkList to enable work pool support.
-
  */
