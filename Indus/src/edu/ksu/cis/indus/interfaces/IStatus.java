194e60fbfb13a5fa1c9e50d266799d7313398969
venku <>
2003-08-21 03:54:41 +0000

    Documentation.

--- Indus/src/edu/ksu/cis/indus/interfaces/IStatus.java
+++ Indus/src/edu/ksu/cis/indus/interfaces/IStatus.java
@@ -40,6 +40,15 @@ package edu.ksu.cis.indus.interfaces;
  * is that the users of information-providing interfaces should be able to check if the object providing that interface is
  * in a stable state to do so.  Hence, it is adviced that information-providing interfaces inherit this interface.  On a
  * general note,  this interface can be used in situations where a support to query status is required.
+ * 
+ * <p>
+ * A note on stability.  In most cases, active entities like analysis may provide different answers for identical queries at
+ * different times when they are active, i.e, when analysis is happening. However, while not active, it will usually provide
+ * the same answer for identical queries at different times.  Nevertheless the answer may be incorrect, but the analysis is
+ * consistent in it's answer. It is this state in which the analysis provide consistent answers that we refer to as stable
+ * state,  a state in which the external behavior of an object will be consistent.  Hence, implementation can use
+ * <code>isStable</code> to indicate/detect activeness.
+ * </p>
  *
  * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
  * @author $Author$
@@ -57,4 +66,7 @@ public interface IStatus {
 /*
    ChangeLog:
    $Log$
+   Revision 1.1  2003/08/21 03:30:34  venku
+   Added a new interface to query stableness of objects.
+   Analyses/Engine/Transformation objects in particular.
  */
