430b3d9c2f27d0fc966d287fa556bcd7f7d16636
venku <>
2003-08-25 11:25:23 +0000

    Formatting.

--- StaticAnalyses/src-flow/edu/ksu/cis/indus/staticanalyses/flow/TypeBasedFilter.java
+++ StaticAnalyses/src-flow/edu/ksu/cis/indus/staticanalyses/flow/TypeBasedFilter.java
@@ -43,13 +43,14 @@ import soot.Value;
 import edu.ksu.cis.indus.staticanalyses.interfaces.IEnvironment;
 import edu.ksu.cis.indus.staticanalyses.support.Util;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 /**
  * This class filters out values which are not of the same type or a sub type of <code>type</code>.
  *
@@ -59,6 +60,11 @@ import org.apache.commons.logging.LogFactory;
  */
 public class TypeBasedFilter
   implements IValueFilter {
+	/**
+	 * The logger used by instances of this class to log messages.
+	 */
+	private static final Log LOGGER = LogFactory.getLog(TypeBasedFilter.class);
+
 	/**
 	 * The environment in which the analysis happens.
 	 *
@@ -73,7 +79,6 @@ public class TypeBasedFilter
 	 */
 	private final Type type;
 
-    private static final Log LOGGER = LogFactory.getLog(TypeBasedFilter.class);
 	/**
 	 * Creates a new TypeBasedFilter object.
 	 *
@@ -121,16 +126,15 @@ public class TypeBasedFilter
 				result.add(o);
 			}
 		}
-        
-        if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("Filtered " + result + " from " + values + " when type was " + type);
-        }
+
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("Filtered " + result + " from " + values + " when type was " + type);
+		}
 		return result;
 	}
 
 	/**
-	 * Checks if value should be filtered out.  It is filtered out if it is not of the type being monitored by this
-	 * object.
+	 * Checks if value should be filtered out.  It is filtered out if it is not of the type being monitored by this object.
 	 *
 	 * @param value to be filtered out.
 	 *
@@ -147,25 +151,22 @@ public class TypeBasedFilter
 
 /*
    ChangeLog:
-
    $Log$
+   Revision 1.5  2003/08/25 11:24:53  venku
+   Concretized the definition of filter() methods.
    Revision 1.4  2003/08/16 02:50:22  venku
    Spruced up documentation and specification.
    Moved onNewXXX() methods from IFGNode to AbstractFGNode.
-
    Revision 1.3  2003/08/15 04:15:45  venku
    Got the documentation right.
-
    Revision 1.2  2003/08/15 04:07:56  venku
    Spruced up documentation and specification.
    - Important change is that previously all types of retype and nullconstant were let through.
      This is incorrect as there is not type filtering happening.  This has been fixed.  We now
      only let those that are not of the monitored type.
-
    Revision 1.1  2003/08/07 06:40:24  venku
    Major:
     - Moved the package under indus umbrella.
-
    Revision 1.2  2003/05/22 22:18:31  venku
    All the interfaces were renamed to start with an "I".
    Optimizing changes related Strings were made.
