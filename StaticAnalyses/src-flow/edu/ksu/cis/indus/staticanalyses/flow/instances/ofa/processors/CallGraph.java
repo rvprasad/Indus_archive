791ab9f63ba4fd80fc3d0a15b8acc1ac51e89beb
venku <>
2003-09-13 04:24:42 +0000

    - boundary conditions, boundary conditions.  Well, we did not   handle the case when there was no calls in the system. FIXED.

--- StaticAnalyses/src-flow/edu/ksu/cis/indus/staticanalyses/flow/instances/ofa/processors/CallGraph.java
+++ StaticAnalyses/src-flow/edu/ksu/cis/indus/staticanalyses/flow/instances/ofa/processors/CallGraph.java
@@ -1,36 +1,16 @@
 
 /*
  * Indus, a toolkit to customize and adapt Java programs.
- * Copyright (C) 2003, 2004, 2005
- * Venkatesh Prasad Ranganath (rvprasad@cis.ksu.edu)
- * All rights reserved.
+ * Copyright (c) 2003 SAnToS Laboratory, Kansas State University
  *
- * This work was done as a project in the SAnToS Laboratory,
- * Department of Computing and Information Sciences, Kansas State
- * University, USA (http://indus.projects.cis.ksu.edu/).
- * It is understood that any modification not identified as such is
- * not covered by the preceding statement.
- *
- * This work is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This work is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this toolkit; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA  02111-1307, USA.
- *
- * Java is a trademark of Sun Microsystems, Inc.
- *
- * To submit a bug report, send a comment, or get the latest news on
- * this project and other SAnToS projects, please visit the web-site
- *                http://indus.projects.cis.ksu.edu/
+ * This software is licensed under the KSU Open Academic License.
+ * You should have received a copy of the license with the distribution.
+ * A copy can be found at
+ *     http://www.cis.ksu.edu/santos/license.html
+ * or you can contact the lab at:
+ *     SAnToS Laboratory
+ *     234 Nichols Hall
+ *     Manhattan, KS 66506, USA
  */
 
 package edu.ksu.cis.indus.staticanalyses.flow.instances.ofa.processors;
@@ -589,7 +569,14 @@ public class CallGraph
 			}
 		}
 
-		// calculate heads, recursion roots, and cycle information.
+		// populate the caller2callees with head information in cases where there are no calls in the system.
+		if (caller2callees.isEmpty()) {
+			for (Iterator i = heads.iterator(); i.hasNext();) {
+				caller2callees.put(i.next(), Collections.EMPTY_LIST);
+			}
+		}
+
+		// calculate recursion roots and cycle information.
 		graphCache = new SimpleNodeGraph();
 
 		if (LOGGER.isDebugEnabled()) {
@@ -777,6 +764,10 @@ public class CallGraph
 /*
    ChangeLog:
    $Log$
+   Revision 1.15  2003/09/12 01:24:12  venku
+   - As preprocessing before CallGraph exists happen based on the
+     parts of the system touched.  Non-reachable methods will be
+     pre-processed.  However, later they should be pruned away. FIXED.
    Revision 1.14  2003/09/08 02:07:44  venku
    - debug stmt error. FIXED.
    Revision 1.13  2003/08/25 09:31:39  venku
