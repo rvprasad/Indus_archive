d9a956abc04e2e3b458472143fd1d1079922beb3
venku <>
2003-09-26 23:46:59 +0000

    - Renamed Tool to AbstractTool - Renamed ToolConfiguration to AbstractToolConfiguration - Renamed ToolConfigurator to AbstractToolConfigurator

---
+++ Indus/src/edu/ksu/cis/indus/tools/AbstractTool.java
@@ -0,0 +1,215 @@
+
+/*
+ * Indus, a toolkit to customize and adapt Java programs.
+ * Copyright (c) 2003 SAnToS Laboratory, Kansas State University
+ *
+ * This software is licensed under the KSU Open Academic License.
+ * You should have received a copy of the license with the distribution.
+ * A copy can be found at
+ *     http://www.cis.ksu.edu/santos/license.html
+ * or you can contact the lab at:
+ *     SAnToS Laboratory
+ *     234 Nichols Hall
+ *     Manhattan, KS 66506, USA
+ */
+
+package edu.ksu.cis.indus.tools;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+/**
+ * This is the facade interface exposed by a tool in Indus.  The tool will expose the configurationCollection via
+ * <code>AbstractToolConfiguration</code>, hence, this api forces the tool implementation to handle the interaction with the
+ * environment for issues such as persistence of the configurationCollection.
+ *
+ * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
+ * @author $Author$
+ * @version $Revision$
+ */
+public abstract class AbstractTool {
+	/**
+	 * The logger used by instances of this class to log messages.
+	 */
+	static final Log LOGGER = LogFactory.getLog(AbstractTool.class);
+
+	/**
+	 * This an object used to control the execution of the tool.
+	 */
+	protected final Object control = new Object();
+
+	/**
+	 * This is the configuration information associated with this tool instance.  Subclasses should provide a valid
+	 * reference.
+	 *
+	 * @invariant configurationInfo != null
+	 */
+	protected AbstractToolConfiguration configurationInfo;
+
+	/**
+	 * This is the configurator associated with this tool instance.  Subclasses should provide a valid reference.
+	 *
+	 * @invariant configurator != null
+	 */
+	protected AbstractToolConfigurator configurator;
+
+	/**
+	 * This indicates if the tool should pause execution.
+	 */
+	boolean pause = true;
+
+	/**
+	 * Populate this object with the information in given in string form.
+	 *
+	 * @param stringizedForm contains the information to be loaded into this object.
+	 *
+	 * @pre stringizedForm != null
+	 */
+	public abstract void destringizeConfiguration(final String stringizedForm);
+
+	/**
+	 * Returns a stringized from of the information in the object suitable for serialization.
+	 *
+	 * @return a stringized representation of the infornmation in the collection.
+	 *
+	 * @post result != null
+	 */
+	public abstract String stringizeConfiguration();
+
+	/**
+	 * Retrieves an object that represents the active configuration of the tool.
+	 *
+	 * @return the active configuration of the tool.
+	 *
+	 * @post result != null
+	 */
+	public final AbstractToolConfiguration getActiveConfiguration() {
+		AbstractToolConfiguration result;
+
+		if (configurationInfo instanceof CompositeToolConfiguration) {
+			result = ((CompositeToolConfiguration) configurationInfo).getActiveToolConfiguration();
+		} else {
+			result = configurationInfo;
+		}
+		return result;
+	}
+
+	/**
+	 * Retrieves an editor which enables the user to edit the configuration of the tool.  This can return <code>null</code>,
+	 * if the tool does not have a configurationCollection to edit which is seldom the case.
+	 *
+	 * @return a configurationCollection editor.
+	 */
+	public final AbstractToolConfigurator getConfigurator() {
+		return configurator;
+	}
+
+	/**
+	 * Executes the tool.
+	 *
+	 * @param phase is the suggestive phase to start execution in.
+	 *
+	 * @throws RuntimeException when this method called on a paused tool.
+	 */
+	public final void run(final Object phase) {
+		if (!pause) {
+			Thread i =
+				new Thread() {
+					public void run() {
+						try {
+							execute(phase);
+						} catch (InterruptedException e) {
+							LOGGER.error("InterruptedException occurred.  Resetting the execution pipeline.", e);
+							pause = false;
+						}
+					}
+				};
+			i.start();
+		} else {
+			throw new RuntimeException("run() should be called when the tool is paused.");
+		}
+	}
+
+	/**
+	 * Retursn the current phase in which the tool was executing.
+	 *
+	 * @return the current phase.
+	 */
+	public abstract Object getPhase();
+
+	/**
+	 * Aborts the execution of the tool.
+	 */
+	public final void abort() {
+		// TODO: this needs more support from the underlying framework.
+	}
+
+	/**
+	 * Pauses the execution of the tool.
+	 */
+	public final void pause() {
+		pause = true;
+	}
+
+	/**
+	 * Resumes the execution of the tool.
+	 */
+	public final void resume() {
+		pause = false;
+		control.notify();
+	}
+
+	/**
+	 * Initialize the tool.  This is usually called once on the tool.
+	 */
+	public abstract void initialize();
+
+	/**
+	 * Reset the tool.  All state related information should be erased.
+	 */
+	public abstract void reset();
+
+	/**
+	 * This is the template method in which the actual processing of the tool happens.
+	 *
+	 * @param phase is the suggestive phase to start execution in.
+	 */
+	protected abstract void execute(Object phase)
+	  throws InterruptedException;
+
+	/**
+	 * Used to suspend the tool execution. This indicates that the tool implementation is moving onto a new phase, hence, it
+	 * is at a point where it is safe to  pause/suspend execution.  If the application had requested the tool to pause via
+	 * <code>pause()</code>, this method will suspend the execution of the tool.
+	 *
+	 * @throws InterruptedException when the thread in which the tool has paused is interrupted.
+	 */
+	protected final void movingToNextPhase()
+	  throws InterruptedException {
+		if (pause) {
+			control.wait();
+		}
+	}
+}
+
+/*
+   ChangeLog:
+   $Log$
+   Revision 1.5  2003/09/26 15:00:01  venku
+   - The configuration of tools in Indus has been placed in this package.
+   - Formatting.
+   Revision 1.4  2003/09/26 13:58:43  venku
+   - checkpoint commit.
+   - Renamed ToolConfigurationCollection to CompositeToolConfiguration
+   - Renamed CollectiveToolConfigurator to CompositeToolConfigurator
+   Revision 1.3  2003/09/26 05:56:10  venku
+   - a checkpoint commit.
+   Revision 1.2  2003/09/24 07:03:02  venku
+   - Renamed ToolConfigurationEditor to AbstractToolConfigurator.
+   - Added property id creation support, via factory method, to AbstractToolConfiguration.
+   - Changed the interface in AbstractTool.
+   Revision 1.1  2003/09/24 02:38:55  venku
+   - Added Interfaces to expose the components of Indus as a
+     tool and configure it.
+ */
---
+++ Indus/src/edu/ksu/cis/indus/tools/AbstractToolConfiguration.java
@@ -0,0 +1,152 @@
+
+/*
+ * Indus, a toolkit to customize and adapt Java programs.
+ * Copyright (c) 2003 SAnToS Laboratory, Kansas State University
+ *
+ * This software is licensed under the KSU Open Academic License.
+ * You should have received a copy of the license with the distribution.
+ * A copy can be found at
+ *     http://www.cis.ksu.edu/santos/license.html
+ * or you can contact the lab at:
+ *     SAnToS Laboratory
+ *     234 Nichols Hall
+ *     Manhattan, KS 66506, USA
+ */
+
+package edu.ksu.cis.indus.tools;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+
+/**
+ * This class should be extended by a configurationCollection instance of a tool.  It provides methods to programmatically
+ * configure the tool.  It also provides a method to stringize the configurationCollection if required by the toolkits/IDEs.
+ * Some toolkits/IDE may support persistence of only string-based properties.  To this end, we explicitly provide a
+ * <code>stringizeConfiguration</code> and <code>destringizeConfiguration</code> methods in <code>AbstractTool</code> interface that
+ * should be used to set and get the information in an instance of this class as a string suitable for serialization.
+ *
+ * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
+ * @author $Author$
+ * @version $Revision$
+ */
+public abstract class AbstractToolConfiguration {
+	/**
+	 * The logger used by instances of this class to log messages.
+	 */
+	private static final Log LOGGER = LogFactory.getLog(AbstractToolConfiguration.class);
+
+	/**
+	 * This is set of property ids recognized by this configurationCollection.
+	 */
+	protected final Collection PROPERTY_IDS = new HashSet();
+
+	/**
+	 * This maps properties of this configurationCollection to their values.
+	 *
+	 * @invariant properties != null
+	 */
+	protected final Map properties = new HashMap();
+
+	/**
+	 * <p>
+	 * DOCUMENT ME!
+	 * </p>
+	 */
+	protected String NAME;
+
+	/**
+	 * Sets a property of the configurationCollection. The given <code>propertyID</code> should be a valid property id
+	 * declared in this class.  If not, an exception will be raised.
+	 *
+	 * @param propertyID to be set.
+	 * @param value to be assigned to the property.
+	 *
+	 * @return <code>true</code> if the property was added; <code>false</code>, otherwise.
+	 *
+	 * @throws IllegalArgumentException when an invalid property identifier is specified.
+	 *
+	 * @pre property != null and value != null
+	 */
+	public final boolean setProperty(final Object propertyID, final Object value) {
+		if (!PROPERTY_IDS.contains(propertyID)) {
+			String message = "Invalid property identifier specified: " + propertyID;
+			LOGGER.error(message);
+			throw new IllegalArgumentException(message);
+		}
+
+		boolean result = processProperty(propertyID, value);
+
+		if (result) {
+			properties.put(propertyID, value);
+		}
+		return result;
+	}
+
+	/**
+	 * Retrieves the value of the given property, if it exists.
+	 *
+	 * @param id of the property for which the value is requested.
+	 *
+	 * @return the value of the property if it exists; <code>null</code>, otherwise.
+	 *
+	 * @pre key != null
+	 */
+	public final Object getProperty(final Object id) {
+		return properties.get(id);
+	}
+
+	/**
+	 * DOCUMENT ME!
+	 * 
+	 * <p></p>
+	 */
+	public abstract void initialize();
+
+	/**
+	 * Processes the given property.  This should be overriden by subclasses to handle alter configurationCollection.  Only a
+	 * return value of <code>true</code> will result in the property being added to the configurationCollection.
+	 *
+	 * @param propertyID is the identifier of the property.
+	 * @param value of the property.
+	 *
+	 * @return <code>true</code> if the property was successfully processed; <code>false</code>, otherwise.
+	 *
+	 * @pre propertyID != null
+	 */
+	protected abstract boolean processProperty(final Object propertyID, final Object value);
+}
+
+/*
+   ChangeLog:
+   $Log$
+   Revision 1.7  2003/09/26 15:35:53  venku
+   - finalized methods.
+
+   Revision 1.6  2003/09/26 15:30:39  venku
+   - removed PropertyIdentifier class.
+   - ripple effect of the above change.
+   - formatting
+
+   Revision 1.5  2003/09/26 15:00:01  venku
+   - The configuration of tools in Indus has been placed in this package.
+   - Formatting.
+   Revision 1.4  2003/09/26 13:58:43  venku
+   - checkpoint commit.
+   - Renamed ToolConfigurationCollection to CompositeToolConfiguration
+   - Renamed CollectiveToolConfigurator to CompositeToolConfigurator
+   Revision 1.3  2003/09/26 05:56:10  venku
+   - a checkpoint commit.
+   Revision 1.2  2003/09/24 07:03:02  venku
+   - Renamed ToolConfigurationEditor to AbstractToolConfigurator.
+   - Added property id creation support, via factory method, to AbstractToolConfiguration.
+   - Changed the interface in AbstractTool.
+   Revision 1.1  2003/09/24 02:38:55  venku
+   - Added Interfaces to expose the components of Indus as a
+     tool and configure it.
+ */
