63154e72644280886c7ae3ed48564c05ed4897b6
venku <>
2003-09-28 06:22:54 +0000

    - Added support to plug unit graphs from the environment when   requested by the implementations.

---
+++ Indus/src/edu/ksu/cis/indus/common/CompleteUnitGraphFactory.java
@@ -0,0 +1,61 @@
+
+/*
+ * Indus, a toolkit to customize and adapt Java programs.
+ * Copyright (c) 2003 SAnToS Laboratory, Kansas State University
+ *
+ * This software is licensed under the KSU Open Academic License.
+ * You should have received a copy of the license with the distribution.
+ * A copy can be found at
+ *     http://www.cis.ksu.edu/santos/license.html
+ * or you can contact the lab at:
+ *     SAnToS Laboratory
+ *     234 Nichols Hall
+ *     Manhattan, KS 66506, USA
+ */
+
+package edu.ksu.cis.indus.common;
+
+import soot.SootMethod;
+
+import soot.toolkits.graph.CompleteUnitGraph;
+import soot.toolkits.graph.UnitGraph;
+
+import edu.ksu.cis.indus.interfaces.AbstractUnitGraphFactory;
+
+import java.lang.ref.WeakReference;
+
+
+/**
+ * This class provides <code>soot.toolkits.graph.CompleteUnitGraph</code>s.
+ *
+ * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
+ * @author $Author$
+ * @version $Revision$ $Date$
+ */
+public class CompleteUnitGraphFactory
+  extends AbstractUnitGraphFactory {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @post result != null and result.oclIsKindOf(CompleteUnitGraph)
+	 *
+	 * @see edu.ksu.cis.indus.interfaces.AbstractUnitGraphFactory#getStmtGraph(soot.SootMethod)
+	 */
+	public UnitGraph getStmtGraph(SootMethod method) {
+		WeakReference ref = (WeakReference) method2StmtGraph.get(method);
+		UnitGraph result = null;
+
+		if (ref == null || ref.get() == null) {
+			result = new CompleteUnitGraph(method.retrieveActiveBody());
+			method2StmtGraph.put(method, new WeakReference(result));
+		} else if (ref != null) {
+			result = (CompleteUnitGraph) ref.get();
+		}
+		return result;
+	}
+}
+
+/*
+   ChangeLog:
+   $Log$
+ */
