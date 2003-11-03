60bb63c20ca6fccbb50222ac96b39ff1ca7bf648
venku <>
2003-11-03 07:59:54 +0000

    - formatting.

--- Indus/src/edu/ksu/cis/indus/tools/IToolConfigurationFactory.java
+++ Indus/src/edu/ksu/cis/indus/tools/IToolConfigurationFactory.java
@@ -1,14 +1,44 @@
-package edu.ksu.cis.indus.tools;
-
-
-public interface IToolConfigurationFactory {
-
-    AbstractToolConfiguration createToolConfiguration();
-}
-
 
 /*
-ChangeLog:
+ * Indus, a toolkit to customize and adapt Java programs.
+ * Copyright (c) 2003 SAnToS Laboratory, Kansas State University
+ *
+ * This software is licensed under the KSU Open Academic License.
+ * You should have received a copy of the license with the distribution.
+ * A copy can be found at
+ *     http://www.cis.ksu.edu/santos/license.html
+ * or you can contact the lab at:
+ *     SAnToS Laboratory
+ *     234 Nichols Hall
+ *     Manhattan, KS 66506, USA
+ */
 
-$Log$
-*/
+package edu.ksu.cis.indus.tools;
+
+/**
+ * DOCUMENT ME!
+ * <p></p>
+ * 
+ * @version $Revision$ 
+ * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
+ * @author $Author$
+ */
+public interface IToolConfigurationFactory {
+	/**
+	 * DOCUMENT ME! <p></p>
+	 *
+	 * @return DOCUMENT ME!
+	 */
+	AbstractToolConfiguration createToolConfiguration();
+}
+
+/*
+   ChangeLog:
+   $Log$
+   Revision 1.1  2003/10/20 13:55:25  venku
+   - Added a factory to create new configurations.
+   - Simplified AbstractToolConfigurator methods.
+   - The driver manages the shell.
+   - Got all the gui parts running EXCEPT for changing
+     the name of the configuration.
+ */
