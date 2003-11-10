e888498ed238406490e8017c2f1ec8945994c703
venku <>
2003-11-10 03:12:23 +0000

    - added an abstract implementation of IProcessor.

---
+++ Indus/src/edu/ksu/cis/indus/processing/AbstractProcessor.java
@@ -0,0 +1,87 @@
+
+/*
+ * Indus, a toolkit to customize and adapt Java programs.
+ * Copyright (c) 2003 SAnToS Laboratory, Kansas State University
+ *
+ * This software is licensed under the KSU Open Academic License.
+ * You should have received a copy of the license with the distribution.
+ * A copy can be found at
+ *     http://www.cis.ksu.edu/santos/license.html
+ * or you can contact the lab at:
+ *     SAnToS Laboratory
+ *     234 Nichols Hall
+ *     Manhattan, KS 66506, USA
+ */
+
+package edu.ksu.cis.indus.processing;
+
+import soot.SootClass;
+import soot.SootField;
+import soot.SootMethod;
+import soot.Value;
+
+import soot.jimple.Stmt;
+
+
+/**
+ * Abstract implementation of IProcessor.
+ *
+ * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
+ * @author $Author$
+ * @version $Revision$ $Date$
+ */
+public abstract class AbstractProcessor
+  implements IProcessor {
+	/**
+	 * Does nothing.
+	 *
+	 * @see IProcessor#callback(Value, Context)
+	 */
+	public void callback(final Value value, final Context context) {
+	}
+
+	/**
+	 * Does nothing.
+	 *
+	 * @see IProcessor#callback(Stmt, Context)
+	 */
+	public void callback(final Stmt stmt, final Context context) {
+	}
+
+	/**
+	 * Does nothing.
+	 *
+	 * @see IProcessor#callback(SootMethod)
+	 */
+	public void callback(final SootMethod method) {
+	}
+
+	/**
+	 * Does nothing.
+	 *
+	 * @see IProcessor#callback(SootClass)
+	 */
+	public void callback(final SootClass clazz) {
+	}
+
+	/**
+	 * Does nothing.
+	 *
+	 * @see IProcessor#callback(SootField)
+	 */
+	public void callback(final SootField field) {
+	}
+
+	/**
+	 * Does nothing.
+	 *
+	 * @see IProcessor#consolidate()
+	 */
+	public void consolidate() {
+	}
+}
+
+/*
+   ChangeLog:
+   $Log$
+ */
