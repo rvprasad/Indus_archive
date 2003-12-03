10fc5274743256d2f78a9a9281713a9cdbc4a821
venku <>
2003-12-03 21:44:20 +0000

    - added support for equals() and hashCode() methods.

--- Indus/src/edu/ksu/cis/indus/common/NamedTag.java
+++ Indus/src/edu/ksu/cis/indus/common/NamedTag.java
@@ -60,11 +60,36 @@ public class NamedTag
 	public final byte[] getValue() {
 		return name.getBytes();
 	}
+
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	public boolean equals(final Object o) {
+		boolean result = false;
+
+		if (o instanceof NamedTag) {
+			final NamedTag _t = (NamedTag) o;
+			result = name == _t.name || _t.name.equals(name);
+		}
+		return result;
+	}
+
+	/**
+	 * @see java.lang.Object#hashCode()
+	 */
+	public int hashCode() {
+		int result = 17;
+		result = result * 37 + name.hashCode();
+		return result;
+	}
 }
 
 /*
    ChangeLog:
    $Log$
+   Revision 1.3  2003/12/02 09:42:25  venku
+   - well well well. coding convention and formatting changed
+     as a result of embracing checkstyle 3.2
    Revision 1.2  2003/12/02 01:30:59  venku
    - coding conventions and formatting.
    Revision 1.1  2003/11/30 00:46:58  venku
