e20fcd0e476082c3c463de5cbe82aa3e959df041
venku <>
2003-12-04 09:45:29 +0000

    - added interface and it's abstract implementation to   support pooling of objects.

---
+++ Indus/src/edu/ksu/cis/indus/interfaces/AbstractPoolable.java
@@ -0,0 +1,76 @@
+
+/*
+ * Indus, a toolkit to customize and adapt Java programs.
+ * Copyright (c) 2003 SAnToS Laboratory, Kansas State University
+ *
+ * This software is licensed under the KSU Open Academic License.
+ * You should have received a copy of the license with the distribution.
+ * A copy can be found at
+ *     http://www.cis.ksu.edu/santos/license.html
+ * or you can contact the lab at:
+ *     SAnToS Laboratory
+ *     234 Nichols Hall
+ *     Manhattan, KS 66506, USA
+ */
+
+package edu.ksu.cis.indus.interfaces;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.apache.commons.pool.ObjectPool;
+
+
+/**
+ * This provides an abstract implementation of <code>IPoolable</code>.  Concrete implementation should extend this class.
+ *
+ * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
+ * @author $Author$
+ * @version $Revision$ $Date$
+ */
+public abstract class AbstractPoolable {
+	/**
+	 * The logger used by instances of this class to log messages.
+	 */
+	private static final Log LOGGER = LogFactory.getLog(AbstractPoolable.class);
+
+	/**
+	 * The pool to which this object should be returned to.
+	 */
+	private ObjectPool pool;
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @throws IllegalArgumentException when <code>thePool</code> is <code>null</code>.
+	 */
+	public final void setPool(final ObjectPool thePool) {
+		if (thePool == null) {
+			throw new IllegalArgumentException("Invalid argument: null");
+		}
+		pool = thePool;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @throws RuntimeException if the returning of the object to it's pool failed.
+	 */
+	public final void returnToPool() {
+		if (pool != null) {
+			try {
+				pool.returnObject(this);
+			} catch (Exception e) {
+				if (LOGGER.isWarnEnabled()) {
+					LOGGER.warn("How can this happen?", e);
+				}
+				throw new RuntimeException(e);
+			}
+		}
+	}
+}
+
+/*
+   ChangeLog:
+   $Log$
+ */
---
+++ Indus/src/edu/ksu/cis/indus/interfaces/IPoolable.java
@@ -0,0 +1,47 @@
+
+/*
+ * Indus, a toolkit to customize and adapt Java programs.
+ * Copyright (c) 2003 SAnToS Laboratory, Kansas State University
+ *
+ * This software is licensed under the KSU Open Academic License.
+ * You should have received a copy of the license with the distribution.
+ * A copy can be found at
+ *     http://www.cis.ksu.edu/santos/license.html
+ * or you can contact the lab at:
+ *     SAnToS Laboratory
+ *     234 Nichols Hall
+ *     Manhattan, KS 66506, USA
+ */
+
+package edu.ksu.cis.indus.interfaces;
+
+import org.apache.commons.pool.ObjectPool;
+
+
+/**
+ * This is a generic interface to be implemented by poolable objects.
+ * 
+ * @version $Revision$ 
+ * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
+ * @author $Author$
+ */
+public interface IPoolable {
+	/**
+	 * Sets the pool to which this object should be returned to.
+	 *
+	 * @param thePool to which this object should be returned.
+	 *
+	 * @pre thePool != null
+	 */
+	void setPool(final ObjectPool thePool);
+
+	/**
+	 * Returns the object to it's pool.
+	 */
+	void returnToPool();
+}
+
+/*
+   ChangeLog:
+   $Log$
+ */
