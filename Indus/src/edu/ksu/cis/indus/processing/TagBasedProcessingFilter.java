f939e72bf0e790c99eda2ff5da3af5def1ff5e35
venku <>
2003-12-05 12:43:22 +0000

    - logging.

--- Indus/src/edu/ksu/cis/indus/processing/TagBasedProcessingFilter.java
+++ Indus/src/edu/ksu/cis/indus/processing/TagBasedProcessingFilter.java
@@ -20,6 +20,11 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.collections.CollectionUtils;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import soot.SootClass;
 import soot.SootMethod;
 
@@ -33,6 +38,11 @@ import soot.SootMethod;
  */
 public class TagBasedProcessingFilter
   extends AbstractProcessingFilter {
+	/**
+	 * The logger used by instances of this class to log messages.
+	 */
+	private static final Log LOGGER = LogFactory.getLog(TagBasedProcessingFilter.class);
+
 	/**
 	 * The name of the tag used to filter out classes and methods.
 	 */
@@ -62,6 +72,14 @@ public class TagBasedProcessingFilter
 				_result.add(_sc);
 			}
 		}
+
+		if (LOGGER.isDebugEnabled()) {
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Filtered out: " + CollectionUtils.subtract(classes, _result));
+				LOGGER.debug("Retained : " + _result);
+			}
+		}
+
 		return _result;
 	}
 
@@ -78,6 +96,14 @@ public class TagBasedProcessingFilter
 				_result.add(_sc);
 			}
 		}
+
+		if (LOGGER.isDebugEnabled()) {
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Filtered out: " + CollectionUtils.subtract(methods, _result));
+				LOGGER.debug("Retained : " + _result);
+			}
+		}
+
 		return _result;
 	}
 }
@@ -85,6 +111,9 @@ public class TagBasedProcessingFilter
 /*
    ChangeLog:
    $Log$
+   Revision 1.4  2003/12/02 11:31:57  venku
+   - Added Interfaces for ToolConfiguration and ToolConfigurator.
+   - coding convention and formatting.
    Revision 1.3  2003/12/02 09:42:25  venku
    - well well well. coding convention and formatting changed
      as a result of embracing checkstyle 3.2
