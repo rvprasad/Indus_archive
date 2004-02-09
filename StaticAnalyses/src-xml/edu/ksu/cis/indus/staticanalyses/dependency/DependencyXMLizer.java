332205c365c6fc0ff8b8bf2d6b899ad38684e7af
venku <>
2004-02-09 07:46:37 +0000

    - added new class to xmlize OFA info.

--- StaticAnalyses/src-xml/edu/ksu/cis/indus/staticanalyses/dependency/DependencyXMLizer.java
+++ StaticAnalyses/src-xml/edu/ksu/cis/indus/staticanalyses/dependency/DependencyXMLizer.java
@@ -10,7 +10,12 @@ public class DependencyXMLizer extends AbstractXMLizer {
      * @see edu.ksu.cis.indus.xmlizer.AbstractXMLizer#writeXML(java.util.Map)
      */
     public void writeXML(Map info) {
-        // TODO: Auto-generated method stub
+        /*
+         * requires environment, call graph, list of DAs, 
+         * create call graph based processing controller
+         * Create a stmt xmlizer for each DA, and drive the bunch via the controller.
+         */ 
+        
 
     }
 
@@ -21,4 +26,7 @@ public class DependencyXMLizer extends AbstractXMLizer {
 ChangeLog:
 
 $Log$
+Revision 1.5  2004/02/09 06:49:02  venku
+- deleted dependency xmlization and test classes.
+
 */
---
+++ StaticAnalyses/src-xml/edu/ksu/cis/indus/staticanalyses/flow/instances/ofa/OFAXMLizer.java
@@ -0,0 +1,32 @@
+package edu.ksu.cis.indus.staticanalyses.flow.instances.ofa;
+
+import edu.ksu.cis.indus.xmlizer.AbstractXMLizer;
+
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+public class OFAXMLizer extends AbstractXMLizer {
+
+    /**
+     * @see edu.ksu.cis.indus.xmlizer.AbstractXMLizer#writeXML(java.util.Map)
+     */
+    public void writeXML(Map info) {
+        /*
+         * requires an environment, 
+         * create a tag based processing controller,
+         * use the controller to drive the xmlizer generate the xml for each value box in each statement.
+         */
+
+    }
+
+}
+
+
+/*
+ChangeLog:
+
+$Log$
+*/
