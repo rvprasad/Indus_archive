e7fc469f41c77eb1b10e3330c4110ddacabcaadc
venku <>
2004-04-22 08:23:43 +0000

    - DetailedDiff causes the test cases to fail.  FIXED.

--- Indus/src-test/edu/ksu/cis/indus/AbstractXMLBasedTest.java
+++ Indus/src-test/edu/ksu/cis/indus/AbstractXMLBasedTest.java
@@ -34,7 +34,6 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import org.custommonkey.xmlunit.DetailedDiff;
 import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.ElementNameAndAttributeQualifier;
 import org.custommonkey.xmlunit.XMLTestCase;
@@ -150,9 +149,9 @@ public abstract class AbstractXMLBasedTest
 		try {
 			final Reader _current = new FileReader(new File(_outfileName));
 			final Reader _previous = new FileReader(new File(xmlControlDir + File.separator + getFileName()));
-			final Diff _diff = new DetailedDiff(new Diff(_previous, _current));
+			final Diff _diff = new Diff(_previous, _current);
 			_diff.overrideElementQualifier(new ElementNameAndAttributeQualifier());
-            assertTrue(_diff.toString(),_diff.similar());
+            assertTrue(_diff.toString(), _diff.similar());
 		} catch (IOException _e) {
 			LOGGER.error("Failed to read the xml file " + _outfileName, _e);
 			fail(_e.getMessage());
@@ -248,6 +247,9 @@ public abstract class AbstractXMLBasedTest
 /*
    ChangeLog:
    $Log$
+   Revision 1.16  2004/04/21 09:08:16  venku
+   - used detailed diff output.
+
    Revision 1.15  2004/04/21 08:25:05  venku
    - used a simple interface to check xml document similarity.
 
