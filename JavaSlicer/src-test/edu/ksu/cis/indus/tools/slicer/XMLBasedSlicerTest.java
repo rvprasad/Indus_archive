03639a2f12ee52afe934c9338444214eb69bd14f
venku <>
2004-05-04 09:57:38 +0000

    - coding convention.

--- JavaSlicer/src-test/edu/ksu/cis/indus/tools/slicer/XMLBasedSlicerTest.java
+++ JavaSlicer/src-test/edu/ksu/cis/indus/tools/slicer/XMLBasedSlicerTest.java
@@ -68,6 +68,9 @@ public final class XMLBasedSlicerTest
 /*
    ChangeLog:
    $Log$
+   Revision 1.2  2004/04/20 06:53:15  venku
+   - documentation.
+
    Revision 1.1  2004/04/18 08:59:00  venku
    - enabled test support for slicer.
    Revision 1.8  2004/03/21 20:13:17  venku
@@ -75,41 +78,41 @@ public final class XMLBasedSlicerTest
    Revision 1.7  2004/03/03 08:06:18  venku
    - renamed SliceXMLizer to SliceXMLizerCLI.
    Revision 1.6  2004/02/09 06:49:27  venku
- *** empty log message ***
-               Revision 1.5  2004/01/09 07:02:12  venku
-               - Made -o mandatory in SliceDriver.
-               - all information is dumped into directory specified via -o.
-               - Renamed SliceDriver to SliceXMLizerCLI.
-               Revision 1.4  2003/12/13 02:29:16  venku
-               - Refactoring, documentation, coding convention, and
-                 formatting.
-               Revision 1.3  2003/12/02 09:42:18  venku
-               - well well well. coding convention and formatting changed
-                 as a result of embracing checkstyle 3.2
-               Revision 1.2  2003/11/24 10:14:39  venku
-               - there are no residualizers now.  There is a very precise
-                 slice collector which will collect the slice via tags.
-               - architectural change. The slicer is hard-wired wrt to
-                 slice collection.  Residualization is outside the slicer.
-               Revision 1.1  2003/11/17 03:22:55  venku
-               - added junit test support for Slicing.
-               - refactored code in test for dependency to make it more
-                 simple.
-               Revision 1.6  2003/11/16 19:01:33  venku
-               - documentation.
-               Revision 1.5  2003/11/16 18:41:18  venku
-               - renamed UniqueIDGenerator to UniqueJimpleIDGenerator.
-               Revision 1.4  2003/11/12 10:45:36  venku
-               - soot class path can be set in SootBasedDriver.
-               - dependency tests are xmlunit based.
-               Revision 1.3  2003/11/12 05:18:54  venku
-               - moved xmlizing classes to a different class.
-               Revision 1.2  2003/11/12 05:05:45  venku
-               - Renamed SootDependentTest to SootBasedDriver.
-               - Switched the contents of DependencyXMLizerDriver and XMLBasedSlicerTest.
-               - Corrected errors which emitting xml tags.
-               - added a scrapbook.
-               Revision 1.1  2003/11/11 10:11:27  venku
-               - in the process of making XMLization a user
-                 application and at the same time a tester application.
+   empty log message 
+   Revision 1.5  2004/01/09 07:02:12  venku
+   - Made -o mandatory in SliceDriver.
+   - all information is dumped into directory specified via -o.
+   - Renamed SliceDriver to SliceXMLizerCLI.
+   Revision 1.4  2003/12/13 02:29:16  venku
+   - Refactoring, documentation, coding convention, and
+     formatting.
+   Revision 1.3  2003/12/02 09:42:18  venku
+   - well well well. coding convention and formatting changed
+     as a result of embracing checkstyle 3.2
+   Revision 1.2  2003/11/24 10:14:39  venku
+   - there are no residualizers now.  There is a very precise
+     slice collector which will collect the slice via tags.
+   - architectural change. The slicer is hard-wired wrt to
+     slice collection.  Residualization is outside the slicer.
+   Revision 1.1  2003/11/17 03:22:55  venku
+   - added junit test support for Slicing.
+   - refactored code in test for dependency to make it more
+     simple.
+   Revision 1.6  2003/11/16 19:01:33  venku
+   - documentation.
+   Revision 1.5  2003/11/16 18:41:18  venku
+   - renamed UniqueIDGenerator to UniqueJimpleIDGenerator.
+   Revision 1.4  2003/11/12 10:45:36  venku
+   - soot class path can be set in SootBasedDriver.
+   - dependency tests are xmlunit based.
+   Revision 1.3  2003/11/12 05:18:54  venku
+   - moved xmlizing classes to a different class.
+   Revision 1.2  2003/11/12 05:05:45  venku
+   - Renamed SootDependentTest to SootBasedDriver.
+   - Switched the contents of DependencyXMLizerDriver and XMLBasedSlicerTest.
+   - Corrected errors which emitting xml tags.
+   - added a scrapbook.
+   Revision 1.1  2003/11/11 10:11:27  venku
+   - in the process of making XMLization a user
+     application and at the same time a tester application.
  */
