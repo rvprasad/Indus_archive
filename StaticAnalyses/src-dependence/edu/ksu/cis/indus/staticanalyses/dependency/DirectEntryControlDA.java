48379cd32c0d7387f37d1a5d776fc033bf06b789
venku <>
2004-06-06 02:28:50 +0000

    - INTERIM : still implementating direct control dependence calculation.

--- StaticAnalyses/src-dependence/edu/ksu/cis/indus/staticanalyses/dependency/DirectEntryControlDA.java
+++ StaticAnalyses/src-dependence/edu/ksu/cis/indus/staticanalyses/dependency/DirectEntryControlDA.java
@@ -15,31 +15,142 @@
 
 package edu.ksu.cis.indus.staticanalyses.dependency;
 
+import edu.ksu.cis.indus.common.graph.INode;
+
 import java.util.BitSet;
 import java.util.Collection;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Iterator;
+
 
 /**
- * DOCUMENT ME!
- * <p></p>
+ * This class provides direct entry-based intraprocedural control dependence information.
  *
  * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
  * @author $Author$
  * @version $Revision$ $Date$
+ *
+ * @see EntryControlDA
  */
 public class DirectEntryControlDA
   extends EntryControlDA {
-	/**
-	 * @see EntryControlDA#propagateInfoToSuccs(BitSet, Collection, int, BitSet[][], List)
+	/*
+	 * In this class, instead of propagating all tokens (as done in EntryControlDA), only tokens corresponding to nodes with
+	 * multiple children are propagated to their children.  Only when a node accumulates all tokens of a control point node,
+	 * the tokens at the control point are injected into the token set of the node (accumulateTokensAtNode)
 	 */
-	protected Collection propagateInfoToSuccs(final BitSet parentNodeTokenSet, final Collection succsOf, final int nodeIndex,
-		final BitSet[][] tokenSets, final List nodes) {
-		// TODO: Auto-generated method stub
-		return super.propagateInfoToSuccs(parentNodeTokenSet, succsOf, nodeIndex, tokenSets, nodes);
+
+	/**
+	 * @see EntryControlDA#processNode(INode,BitSet[][])
+	 */
+	protected Collection processNode(final INode node, final BitSet[][] tokenSets) {
+		accumulateTokensAtNode(node, tokenSets);
+
+		final Collection _result;
+
+		if (!nodesWithChildrenCache.contains(node)) {
+			_result = super.processNode(node, tokenSets);
+		} else {
+			/*
+			 * Say B and C are control points, A is the control merge point of B, and A and B are dependent on C.
+			 * It is possible that node A accumulated all tokens from control point B when tokens from control point C had
+			 * not reached B.  In such cases, we need to add nodes such as A to the workbag for processing.
+			 */
+			_result = new HashSet();
+
+			final int _iEnd = nodesCache.size();
+			final int _nodeIndex = nodesCache.indexOf(node);
+			final int _succsSize = node.getSuccsOf().size();
+
+			for (int _iIndex = 0; _iIndex < _iEnd; _iIndex++) {
+				final BitSet _bitSet = tokenSets[_iIndex][_nodeIndex];
+
+				if (_bitSet != null && _bitSet.cardinality() == _succsSize) {
+					_result.add(nodesCache.get(_iIndex));
+				}
+			}
+		}
+		return _result;
+	}
+
+	/**
+	 * Accumulates the tokens of ancestor nodes for the purpose of direct CD calculation.  In this method,  the tokens at
+	 * ancestors of the control points which were dependees for the given node are injected into the token set of the given
+	 * node if the dependees are no longer dependees.
+	 *
+	 * @param node at which to accumulate tokens.
+	 * @param tokenSets is the collection of token sets of the nodes in the graph.
+	 *
+	 * @return <code>true</code> if new tokens were accumulated at <code>node</code>; <code>false</code>, otherwise.
+	 *
+	 * @pre node != null and tokenSets != null
+	 */
+	private boolean accumulateTokensAtNode(final INode node, final BitSet[][] tokenSets) {
+		final int _nodeIndex = nodesCache.indexOf(node);
+		final Iterator _i = nodesWithChildrenCache.iterator();
+		final int _iEnd = nodesWithChildrenCache.size();
+		boolean _result = false;
+
+		for (int _iIndex = 0; _iIndex < _iEnd; _iIndex++) {
+			final INode _cpNode = (INode) _i.next();
+			final int _cpNodeIndex = nodesCache.indexOf(_cpNode);
+			final BitSet _nodeAndCPBitSet = tokenSets[_nodeIndex][_cpNodeIndex];
+
+			if (_nodeAndCPBitSet != null && _nodeAndCPBitSet.cardinality() == _cpNode.getSuccsOf().size()) {
+				_result = _result || copyAncestorBitSetsFromTo(_cpNodeIndex, _nodeIndex, tokenSets);
+			}
+		}
+		return _result;
+	}
+
+	/**
+	 * Injects the tokens corresponding to the ancestors of the node at <code>src</code> into the token sets corresponding to
+	 * the same  ancestors at the node at <code>dest</code>.
+	 *
+	 * @param src is the index of the node whose ancestor's tokens need to be propagated.
+	 * @param dest is the index of the node into which the tokens will be propagated to.
+	 * @param tokenSets is the collection of token sets of the nodes in the graph.
+	 *
+	 * @return <code>true</code> if any new tokens were injected into the token sets of node indicated by <code>dest</code>;
+	 * 		   <code>false</code>, otherwise.
+	 *
+	 * @pre tokenSets != null
+	 * @pre 0 &lt;= src &lt; tokensSets.length
+	 * @pre 0 &lt;= dest &lt; tokensSets.length
+	 */
+	private boolean copyAncestorBitSetsFromTo(final int src, final int dest, final BitSet[][] tokenSets) {
+		final BitSet[] _srcBitSets = tokenSets[src];
+		final BitSet[] _destBitSets = tokenSets[dest];
+		final BitSet _temp = new BitSet(1);
+		boolean _result = false;
+		final Iterator _i = nodesWithChildrenCache.iterator();
+		final int _iEnd = nodesWithChildrenCache.size();
+
+		for (int _iIndex = 0; _iIndex < _iEnd; _iIndex++) {
+			final INode _ancestor = (INode) _i.next();
+			final int _ancestorIndex = nodesCache.indexOf(_ancestor);
+			final BitSet _srcAndAncestorBitSet = _srcBitSets[_ancestorIndex];
+
+			if (_srcAndAncestorBitSet != null) {
+				final BitSet _destAndAncestorBitSet = getBitSetAtLocation(_destBitSets, _ancestorIndex);
+				_temp.clear();
+				_temp.or(_srcAndAncestorBitSet);
+				_temp.andNot(_destAndAncestorBitSet);
+				_result = _result || _temp.cardinality() > 0;
+				_destAndAncestorBitSet.or(_srcAndAncestorBitSet);
+			}
+		}
+		return _result;
 	}
 }
 
 /*
    ChangeLog:
    $Log$
+   Revision 1.1  2004/06/05 09:52:24  venku
+   - INTERIM COMMIT
+     - Reimplemented EntryControlDA.  It provides indirect control dependence info.
+     - DirectEntryControlDA provides direct control dependence info.
+     - ExitControlDA will follow same suite as EntryControlDA with new implementation
+       and new class for direct dependence.
  */
