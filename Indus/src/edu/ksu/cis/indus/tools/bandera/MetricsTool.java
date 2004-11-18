7411763cc8c2f114ddca12ba23838d12e10daeef
venku <>
2004-11-18 01:21:03 +0000

    - unused imports.

--- Indus/src/edu/ksu/cis/indus/tools/bandera/MetricsTool.java
+++ Indus/src/edu/ksu/cis/indus/tools/bandera/MetricsTool.java
@@ -31,9 +31,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.TransformerUtils;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
