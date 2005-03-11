9e74a4b111b66f0797ee41d0d9c73a1000f4f430
rvprasad <>
2005-03-11 01:28:11 +0000

    - Added facility to filter application classes.

---
+++ Indus/src/edu/ksu/cis/indus/common/soot/ApplicationClassesOnlyPredicate.java
@@ -0,0 +1,52 @@
+
+/*
+ * Indus, a toolkit to customize and adapt Java programs.
+ * Copyright (c) 2003, 2004, 2005 SAnToS Laboratory, Kansas State University
+ *
+ * This software is licensed under the KSU Open Academic License.
+ * You should have received a copy of the license with the distribution.
+ * A copy can be found at
+ *     http://www.cis.ksu.edu/santos/license.html
+ * or you can contact the lab at:
+ *     SAnToS Laboratory
+ *     234 Nichols Hall
+ *     Manhattan, KS 66506, USA
+ */
+
+package edu.ksu.cis.indus.common.soot;
+
+import org.apache.commons.collections.Predicate;
+
+import soot.SootClass;
+import soot.SootMethod;
+
+
+/**
+ * This predicate can be used to check if a given class is or method belongs to an application class.
+ *
+ * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
+ * @author $Author$
+ * @version $Revision$ $Date$
+ */
+public class ApplicationClassesOnlyPredicate
+  implements Predicate {
+	/**
+	 * @see Predicate#evaluate(java.lang.Object)
+	 */
+	public boolean evaluate(final Object object) {
+		final boolean _result;
+
+		if (object instanceof SootMethod) {
+			final SootMethod _sm = (SootMethod) object;
+			_result = _sm.getDeclaringClass().isApplicationClass();
+		} else if (object instanceof SootClass) {
+			final SootClass _sc = (SootClass) object;
+			_result = _sc.isApplicationClass();
+		} else {
+			_result = true;
+		}
+		return _result;
+	}
+}
+
+// End of File
