0b4a54e293e0d03dff7211fdec18c19b4774d45a
rvprasad <>
2005-05-08 18:48:11 +0000

    - error in getDependees. FIXED. - added a toString(Collection) method.

--- StaticAnalyses/src-dependence/edu/ksu/cis/indus/staticanalyses/dependency/IndirectDependenceAnalysis.java
+++ StaticAnalyses/src-dependence/edu/ksu/cis/indus/staticanalyses/dependency/IndirectDependenceAnalysis.java
@@ -24,7 +24,11 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+
+import soot.SootMethod;
 
 
 /**
@@ -43,11 +47,6 @@ final class IndirectDependenceAnalysis
 	private final Collection processedCache = new HashSet();
 
 	/** 
-	 * This is a cache variable for work bag.
-	 */
-	private final IWorkBag wbCache = new HistoryAwareLIFOWorkBag(processedCache);
-
-	/** 
 	 * This retrieves dependence from the given analysis.
 	 */
 	private final IDependenceRetriever retriever;
@@ -56,6 +55,11 @@ final class IndirectDependenceAnalysis
 	 * This analysis provides seed dependence information.
 	 */
 	private final IDependencyAnalysis da;
+
+	/** 
+	 * This is a cache variable for work bag.
+	 */
+	private final IWorkBag wbCache = new HistoryAwareLIFOWorkBag(processedCache);
 
 	/** 
 	 * This is similar to <code>dependent2dependee</code> except the direction is dependee->dependent. Hence, it is
@@ -96,7 +100,7 @@ final class IndirectDependenceAnalysis
 		if (_result == null) {
 			processedCache.clear();
 			wbCache.clear();
-			wbCache.addAllWork(da.getDependents(dependent, context));
+			wbCache.addAllWork(da.getDependees(dependent, context));
 
 			while (wbCache.hasWork()) {
 				final Object _dependence = wbCache.getWork();
@@ -164,6 +168,50 @@ final class IndirectDependenceAnalysis
 	public void reset() {
 		dependee2dependent.clear();
 		dependent2dependee.clear();
+	}
+
+	/**
+	 * Returns a stringized representation of this analysis.  The representation includes the results of the analysis.
+	 *
+	 * @param methods for which the information needs to be stringized.
+	 *
+	 * @return a stringized representation of this object.
+	 *
+	 * @pre methods != null and methods.oclIsKindOf(Collection(SootMethod))
+	 * @post result != null
+	 */
+	public String toString(final Collection methods) {
+		final StringBuffer _result =
+			new StringBuffer("Statistics for control dependence as calculated by " + getClass().getName() + "\n");
+		int _localEdgeCount;
+		int _edgeCount = 0;
+
+		final StringBuffer _temp = new StringBuffer();
+
+		for (final Iterator _i = methods.iterator(); _i.hasNext();) {
+			final SootMethod _method = (SootMethod) _i.next();
+
+			if (!_method.hasActiveBody()) {
+				continue;
+			}
+
+			final List _stmts = new ArrayList(_method.getActiveBody().getUnits());
+			_localEdgeCount = 0;
+
+			for (int _j = 0; _j < _stmts.size(); _j++) {
+				final Object _stmt = _stmts.get(_j);
+				final Collection _dees = getDependees(_stmt, _method);
+				_temp.append("\t\t" + _stmt + " --> " + _dees + "\n");
+				_localEdgeCount += _dees.size();
+			}
+
+			_result.append("\tFor " + _method + " there are " + _localEdgeCount + " control dependence edges.\n");
+			_result.append(_temp);
+			_temp.delete(0, _temp.length());
+			_edgeCount += _localEdgeCount;
+		}
+		_result.append("A total of " + _edgeCount + " control dependence edges exists.\n");
+		return _result.toString();
 	}
 }
 
