3d96aaf85f6c0d223215e4ead388995a745d2959
rvprasad <>
2005-05-09 15:00:34 +0000

    - added a new class to perform throw analysis.  Still needs to be implemented. TODO.

---
+++ StaticAnalyses/src/edu/ksu/cis/indus/staticanalyses/cfg/ThrowAnalysis.java
@@ -0,0 +1,21 @@
+package edu.ksu.cis.indus.staticanalyses.cfg;
+
+import edu.ksu.cis.indus.staticanalyses.interfaces.AbstractAnalysis;
+
+
+public class ThrowAnalysis extends AbstractAnalysis {
+
+    public ThrowAnalysis() {
+        super();
+        // TODO: Auto-generated constructor stub
+    }
+
+    public void analyze() {
+        // TODO: Auto-generated method stub
+
+    }
+
+}
+
+
+// End of File
