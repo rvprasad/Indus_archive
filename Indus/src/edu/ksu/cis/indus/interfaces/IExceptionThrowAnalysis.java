f4095f7f6735daae247f064d3a1f1d6d71a5406a
rvprasad <>
2005-05-13 06:12:14 +0000

    - added a new interface to extract thrown exception information - IExceptionThrowAnalysis.

---
+++ Indus/src/edu/ksu/cis/indus/interfaces/IExceptionThrowAnalysis.java
@@ -0,0 +1,81 @@
+
+/*
+ * Indus, a toolkit to customize and adapt Java programs.
+ * Copyright (c) 2003, 2004, 2005 SAnToS Laboratory, Kansas State University
+ *
+ * This software is licensed under the KSU Open Academic License.
+ * You should have received a copy of the license with the distribution.
+ * A copy can be found at
+ *     http://www.cis.ksu.edu/santos/license.html
+ * or you can contact the lab at:
+ *     SAnToS Laboratory
+ *     234 Nichols Hall
+ *     Manhattan, KS 66506, USA
+ */
+
+package edu.ksu.cis.indus.interfaces;
+
+import java.util.Collection;
+
+import soot.SootMethod;
+
+import soot.jimple.Stmt;
+
+
+/**
+ * This is the interface to access thrown exception information. 
+ *
+ * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
+ * @author $Author$
+ * @version $Revision$
+ */
+public interface IExceptionThrowAnalysis
+  extends IIdentification,
+	  IStatus {
+	/** 
+	 * This is the unique identifier that can be used to identify an instance of this class.
+	 */
+	Object ID = "exception throw Information";
+
+	/**
+	 * Retrieves the exceptions thrown by the given statement in the given method.
+	 *
+	 * @param stmt of interest.
+	 * @param method in which <code>stmt</code> occurs.
+	 *
+	 * @return a collection of exception types.
+	 *
+	 * @pre stmt != null and method != null
+	 * @post result != null and result.oclIsKindOf(Collection(SootClass))
+	 * @post result.containsAll(getUncaughtExceptionsThrownBy(stmt, method))
+	 */
+	Collection getExceptionsThrownBy(final Stmt stmt, final SootMethod method);
+
+	/**
+	 * Retrieves the uncaught exceptions thrown by the given statement in the given method.
+	 *
+	 * @param stmt of interest.
+	 * @param method in which <code>stmt</code> occurs.
+	 *
+	 * @return a collection of exception types.
+	 *
+	 * @pre stmt != null and method != null
+	 * @post result != null and result.oclIsKindOf(Collection(SootClass))
+	 * @post getExceptionsThrownBy(stmt, method).containsAll(result)
+	 */
+	Collection getUncaughtExceptionsThrownBy(final Stmt stmt, final SootMethod method);
+
+	/**
+	 * Checks if the given statement throws an uncaught exception in the given method.
+	 *
+	 * @param stmt of interest.
+	 * @param method in which <code>stmt</code> occurs.
+	 *
+	 * @return <code>true</code> if an uncaught exception will be thrown; <code>false</code>, otherwise.
+	 *
+	 * @pre stmt != null and method != null
+	 */
+	boolean doesStmtThrowUncaughtException(final Stmt stmt, final SootMethod method);
+}
+
+// End of File
