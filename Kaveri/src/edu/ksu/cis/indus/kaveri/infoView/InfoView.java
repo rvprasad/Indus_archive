ee6eddf78394130431af3a8c9c23f140e7c778cd
ganeshan <>
2005-06-03 21:58:20 +0000

    *** empty log message ***

---
+++ Kaveri/src/edu/ksu/cis/indus/kaveri/infoView/InfoView.java
@@ -0,0 +1,45 @@
+/*
+ *
+ * Indus, a toolkit to customize and adapt Java programs.
+ * Copyright (c) 2003 SAnToS Laboratory, Kansas State University
+ *
+ * This software is licensed under the KSU Open Academic License.
+ * You should have received a copy of the license with the distribution.
+ * A copy can be found at
+ *     http://www.cis.ksu.edu/santos/license.html
+ * or you can contact the lab at:
+ *     SAnToS Laboratory
+ *     234 Nichols Hall
+ *     Manhattan, KS 66506, USA
+ */
+ 
+package edu.ksu.cis.indus.kaveri.infoView;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+/**
+ * @author ganeshan
+ *
+ * This view displays the updated information regarding
+ * the scope, contexts and the slice statistics.
+ */
+public class InfoView extends ViewPart {
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IWorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+     */
+    public void createPartControl(Composite parent) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IWorkbenchPart#setFocus()
+     */
+    public void setFocus() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
