372f05db0e03ad671b66374729dc0b4dc1e2c456
rvprasad <>
2005-06-27 19:29:21 +0000

    - Renamed IAtomicityInfo to IConcurrentIndependenceInfo.

--- Indus/src/edu/ksu/cis/indus/interfaces/IConcurrentIndependenceInfo.java
+++ Indus/src/edu/ksu/cis/indus/interfaces/IConcurrentIndependenceInfo.java
@@ -19,29 +19,29 @@ import soot.jimple.Stmt;
 
 
 /**
- * This interface exposes atomicity information.
+ * This interface exposes independence information in the context of concurrency.
  *
  * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
  * @author $Author$
  * @version $Revision$
  */
-public interface IAtomicityInfo
+public interface IConcurrentIndependenceInfo
   extends IIdentification {
 	/** 
 	 * This is the unique identifier that can be used to identify an instance of this class.
 	 */
-	Object ID = "Class Hierarchy Analysis";
+	Object ID = "Concurrent Independence Analysis";
 
 	/**
-	 * Checks if the statement is atomic.
+	 * Checks if the statement is independent.
 	 *
 	 * @param stmt to be checked.
 	 *
-	 * @return <code>true</code> if <code>stmt</code> is atomic; <code>false</code>, otherwise.
+	 * @return <code>true</code> if <code>stmt</code> is independent; <code>false</code>, otherwise.
 	 *
 	 * @pre stmt != null
 	 */
-	boolean isAtomic(final Stmt stmt);
+	boolean isIndependent(final Stmt stmt);
 }
 
 // End of File
