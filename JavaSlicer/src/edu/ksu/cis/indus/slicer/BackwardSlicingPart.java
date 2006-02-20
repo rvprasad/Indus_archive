6ccb2a3b5cb9231818484883ed315f3886ed25f0
rvprasad <>
2006-02-20 06:30:44 +0000

    - minor optimization.

--- JavaSlicer/src/edu/ksu/cis/indus/slicer/BackwardSlicingPart.java
+++ JavaSlicer/src/edu/ksu/cis/indus/slicer/BackwardSlicingPart.java
@@ -20,8 +20,8 @@ import edu.ksu.cis.indus.common.collections.Stack;
 import edu.ksu.cis.indus.common.datastructures.Pair;
 import edu.ksu.cis.indus.common.datastructures.Triple;
 import edu.ksu.cis.indus.common.soot.BasicBlockGraph;
-import edu.ksu.cis.indus.common.soot.BasicBlockGraph.BasicBlock;
 import edu.ksu.cis.indus.common.soot.BasicBlockGraphMgr;
+import edu.ksu.cis.indus.common.soot.BasicBlockGraph.BasicBlock;
 import edu.ksu.cis.indus.interfaces.ICallGraphInfo.CallTriple;
 import edu.ksu.cis.indus.processing.Context;
 import edu.ksu.cis.indus.staticanalyses.cfg.LocalUseDefAnalysisv2;
@@ -56,7 +56,7 @@ import soot.jimple.ThrowStmt;
 
 /**
  * This class provides the logic to detect parts of a backward slice.
- *
+ * 
  * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
  * @author $Author$
  * @version $Revision$ $Date$
@@ -66,7 +66,7 @@ public class BackwardSlicingPart
 
 	/**
 	 * This closure contains logic to generate criteria to include the value in the return statements.
-	 *
+	 * 
 	 * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
 	 * @author $Author$
 	 * @version $Revision$ $Date$
@@ -75,6 +75,8 @@ public class BackwardSlicingPart
 			implements IClosure<Pair<Stmt, SootMethod>> {
 
 		/**
+		 * {@inheritDoc}
+		 * 
 		 * @see edu.ksu.cis.indus.common.collections.IClosure#execute(Object)
 		 */
 		public void execute(final Pair<Stmt, SootMethod> input) {
@@ -91,7 +93,7 @@ public class BackwardSlicingPart
 
 	/**
 	 * This closure contains logic to generate criteria to include return statements.
-	 *
+	 * 
 	 * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
 	 * @author $Author$
 	 * @version $Revision$ $Date$
@@ -100,6 +102,8 @@ public class BackwardSlicingPart
 			implements IClosure<Pair<Stmt, SootMethod>> {
 
 		/**
+		 * {@inheritDoc}
+		 * 
 		 * @see edu.ksu.cis.indus.common.collections.IClosure#execute(Object)
 		 */
 		public void execute(final Pair<Stmt, SootMethod> input) {
@@ -131,7 +135,7 @@ public class BackwardSlicingPart
 
 	/**
 	 * This maps methods to methods to a bitset that indicates which of the parameters of the method is required in the slice.
-	 *
+	 * 
 	 * @invariant method2params.oclIsKindOf(Map(SootMethod, BitSet))
 	 * @invariant method2params->forall(o | o.getValue().size() = o.getKey().getParameterCount())
 	 */
@@ -149,7 +153,7 @@ public class BackwardSlicingPart
 
 	/**
 	 * Creates an instance of this class.
-	 *
+	 * 
 	 * @param theEngine of which this part is a part of.
 	 * @pre theEngine != null
 	 */
@@ -158,6 +162,8 @@ public class BackwardSlicingPart
 	}
 
 	/**
+	 * {@inheritDoc}
+	 * 
 	 * @see edu.ksu.cis.indus.slicer.IDirectionSensitivePartOfSlicingEngine#continueProcessing()
 	 */
 	public boolean continueProcessing() {
@@ -167,27 +173,24 @@ public class BackwardSlicingPart
 
 		boolean _result = false;
 
-		for (final Iterator<Map.Entry<SootMethod, Collection<Triple<Stmt, SootMethod, Stack<CallTriple>>>>> _i = callee2callsites.entrySet().iterator(); _i.hasNext();) {
+		for (final Iterator<Map.Entry<SootMethod, Collection<Triple<Stmt, SootMethod, Stack<CallTriple>>>>> _i = callee2callsites
+				.entrySet().iterator(); _i.hasNext();) {
 			final Map.Entry<SootMethod, Collection<Triple<Stmt, SootMethod, Stack<CallTriple>>>> _entry = _i.next();
-			final SootMethod callee = _entry.getKey();
-			final BitSet _params = method2params.get(callee);
+			final SootMethod _callee = _entry.getKey();
+			final BitSet _params = method2params.get(_callee);
 			if (_params != null) {
 				final Collection<Triple<Stmt, SootMethod, Stack<CallTriple>>> _temp = _entry.getValue();
-				final boolean _remove = _params.cardinality() == callee.getParameterCount();
 				for (final Iterator<Triple<Stmt, SootMethod, Stack<CallTriple>>> _k = _temp.iterator(); _k.hasNext();) {
-					final Triple<Stmt, SootMethod, Stack<CallTriple>> _triple =  _k.next();
+					final Triple<Stmt, SootMethod, Stack<CallTriple>> _triple = _k.next();
 					final Stmt _stmt = _triple.getFirst();
 					final SootMethod _caller = _triple.getSecond();
-					final Stack<CallTriple> _stack =  _triple.getThird();
+					final Stack<CallTriple> _stack = _triple.getThird();
 					final InvokeExpr _expr = _stmt.getInvokeExpr();
 					for (int _j = _params.nextSetBit(0); _j >= 0; _j = _params.nextSetBit(_j + 1)) {
 						_result |= engine.generateExprLevelSliceCriterion(_expr.getArgBox(_j), _stmt, _caller, true, _stack);
 					}
-					if (_remove) {
-						_k.remove();
-					}
 				}
-				if (_temp.isEmpty()) {
+				if (_params.cardinality() == _callee.getParameterCount()) {
 					_i.remove();
 				}
 			}
@@ -201,6 +204,8 @@ public class BackwardSlicingPart
 	}
 
 	/**
+	 * {@inheritDoc}
+	 * 
 	 * @see IDirectionSensitivePartOfSlicingEngine#generateCriteriaForTheCallToMethod(soot.SootMethod, soot.SootMethod,
 	 *      soot.jimple.Stmt)
 	 */
@@ -228,6 +233,8 @@ public class BackwardSlicingPart
 	}
 
 	/**
+	 * {@inheritDoc}
+	 * 
 	 * @see edu.ksu.cis.indus.slicer.IDirectionSensitivePartOfSlicingEngine#generateCriteriaToIncludeCallees(soot.jimple.Stmt,
 	 *      soot.SootMethod, java.util.Collection)
 	 */
@@ -246,6 +253,8 @@ public class BackwardSlicingPart
 	}
 
 	/**
+	 * {@inheritDoc}
+	 * 
 	 * @see DependenceExtractor.IDependenceRetriver#getDependences(IDependencyAnalysis, Object, SootMethod )
 	 */
 	public Collection<Object> getDependences(final IDependencyAnalysis analysis, final Object entity, final SootMethod method) {
@@ -263,6 +272,8 @@ public class BackwardSlicingPart
 	}
 
 	/**
+	 * {@inheritDoc}
+	 * 
 	 * @see edu.ksu.cis.indus.slicer.IDirectionSensitivePartOfSlicingEngine#processLocalAt(Local, soot.jimple.Stmt,
 	 *      soot.SootMethod)
 	 */
@@ -300,7 +311,7 @@ public class BackwardSlicingPart
 
 	/**
 	 * Processes new expressions to include corresponding init statements into the slice.
-	 *
+	 * 
 	 * @param stmt is the statement containing the new expression.
 	 * @param method contains <code>stmt</code>.
 	 * @pre stmt != null and method != null
@@ -342,7 +353,7 @@ public class BackwardSlicingPart
 	 * <p>
 	 * This should be called from within the callee's context (callStack containing the call to the callee).
 	 * </p>
-	 *
+	 * 
 	 * @param stmt is the statement in which <code>pBox</code> occurs.
 	 * @param callee in which<code>stmt</code> occurs.
 	 * @pre method != null and stmt != null
@@ -397,6 +408,8 @@ public class BackwardSlicingPart
 	}
 
 	/**
+	 * {@inheritDoc}
+	 * 
 	 * @see edu.ksu.cis.indus.slicer.IDirectionSensitivePartOfSlicingEngine#reset()
 	 */
 	public void reset() {
@@ -404,6 +417,8 @@ public class BackwardSlicingPart
 	}
 
 	/**
+	 * {@inheritDoc}
+	 * 
 	 * @see IDirectionSensitivePartOfSlicingEngine#retrieveValueBoxesToTransformExpr(ValueBox, Stmt)
 	 */
 	public Collection<ValueBox> retrieveValueBoxesToTransformExpr(final ValueBox valueBox, final Stmt stmt) {
@@ -435,6 +450,8 @@ public class BackwardSlicingPart
 	}
 
 	/**
+	 * {@inheritDoc}
+	 * 
 	 * @see IDirectionSensitivePartOfSlicingEngine#retrieveValueBoxesToTransformStmt(Stmt)
 	 */
 	public Collection<ValueBox> retrieveValueBoxesToTransformStmt(final Stmt stmt) {
@@ -458,7 +475,7 @@ public class BackwardSlicingPart
 	/**
 	 * Checks if the given called method's return points should be considered to generate new slice criterion. The callee
 	 * should be marked as invoked or required before calling this method.
-	 *
+	 * 
 	 * @param callee is the method in question.
 	 * @param expr indicates if the expression in the tail statement should be checked to determine if criteria should be
 	 *            generated.
@@ -498,7 +515,7 @@ public class BackwardSlicingPart
 	 * <p>
 	 * This should be called from the caller's context (callStack containing the call to the callee).
 	 * </p>
-	 *
+	 * 
 	 * @param callStmt at which the invocation occurs.
 	 * @param caller in which the invocation occurs.
 	 * @pre callStmt != null and caller != null and callee != null
@@ -521,7 +538,7 @@ public class BackwardSlicingPart
 
 	/**
 	 * Processes the init call to the super class inside init method.
-	 *
+	 * 
 	 * @param initMethod is the init method.
 	 * @param bbg is the basic block graph of <code>callee</code>.
 	 * @pre initMethod != null and bbg != null and engine.getCallStackCache() != null
@@ -571,7 +588,7 @@ public class BackwardSlicingPart
 	 * <p>
 	 * This should be called from the caller's context (callStack containing the call to the caller).
 	 * </p>
-	 *
+	 * 
 	 * @param callees are the methods called at the statement.
 	 * @param stmt is the statment containing the invocation.
 	 * @param caller is the method in which <code>invocationStmt</code> occurs.
@@ -629,7 +646,7 @@ public class BackwardSlicingPart
 
 	/**
 	 * Records the given callee was called from the given call-site in the current context.
-	 *
+	 * 
 	 * @param stmt containing the call site.
 	 * @param caller containing <code>stmt</code>
 	 * @param callee being called.
