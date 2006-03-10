3886ad759f4b7728f4445237107244a79f5d07c5
rvprasad <>
2006-03-10 21:45:24 +0000

    - variable renaming.

--- StaticAnalyses/src-dependence/edu/ksu/cis/indus/staticanalyses/dependency/IDependenceRetriever.java
+++ StaticAnalyses/src-dependence/edu/ksu/cis/indus/staticanalyses/dependency/IDependenceRetriever.java
@@ -20,7 +20,7 @@ import java.util.Collection;
 
 /**
  * The interaface used to retrieve dependence to calculate indirect dependence from direct dependence.
- *
+ * 
  * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
  * @author $Author$
  * @version $Revision$ $Date$
@@ -35,7 +35,7 @@ public interface IDependenceRetriever<T1, C1, E1, E2, C2, T2> {
 
 	/**
 	 * DOCUMENT ME!
-	 *
+	 * 
 	 * @param dependees DOCUMENT ME!
 	 * @param base DOCUMENT ME!
 	 * @param context DOCUMENT ME!
@@ -45,17 +45,17 @@ public interface IDependenceRetriever<T1, C1, E1, E2, C2, T2> {
 
 	/**
 	 * DOCUMENT ME!
-	 *
+	 * 
 	 * @param dependees DOCUMENT ME!
 	 * @param base DOCUMENT ME!
 	 * @param context DOCUMENT ME!
 	 * @return DOCUMENT ME!
 	 */
-	Collection<Pair<T1, C1>> convertToConformantDependents(final Collection<E1> dependees, final T1 base, final C1 context);
+	Collection<Pair<T1, C1>> convertToConformantDependents(final Collection<E1> dependents, final T1 base, final C1 context);
 
 	/**
 	 * Retrieves the dependees based on <code>dependence</code> from <code>da</code>.
-	 *
+	 * 
 	 * @param da to be used retrieve dependence info
 	 * @param base that serves as the basis for retrieval.
 	 * @param context attached with the base.
@@ -63,12 +63,11 @@ public interface IDependenceRetriever<T1, C1, E1, E2, C2, T2> {
 	 * @pre da != null and dependence != null
 	 * @post result != null
 	 */
-	Collection<E1> getDependees(final IDependencyAnalysis<T1, C1, E1, E2, C2, T2> da, final T1 base,
-			final C1 context);
+	Collection<E1> getDependees(final IDependencyAnalysis<T1, C1, E1, E2, C2, T2> da, final T1 base, final C1 context);
 
 	/**
 	 * Retrieves the dependents based on <code>dependence</code> from <code>da</code>.
-	 *
+	 * 
 	 * @param da to be used retrieve dependence info.
 	 * @param base that serves as the basis for retrieval.
 	 * @param context attached with the base.
@@ -76,8 +75,7 @@ public interface IDependenceRetriever<T1, C1, E1, E2, C2, T2> {
 	 * @pre da != null and dependence != null
 	 * @post result != null
 	 */
-	Collection<T2> getDependents(final IDependencyAnalysis<T1, C1, E1, E2, C2, T2> da, final E2 base,
-			final C2 context);
+	Collection<T2> getDependents(final IDependencyAnalysis<T1, C1, E1, E2, C2, T2> da, final E2 base, final C2 context);
 }
 
 // End of File
