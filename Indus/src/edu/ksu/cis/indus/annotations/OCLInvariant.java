dd0405c75b3f60f70618337c071dfe551f4f610d
rvprasad <>
2006-04-23 19:49:36 +0000

    - added annotation types to capture ocl constraint.

---
+++ Indus/src/edu/ksu/cis/indus/annotations/OCLInvariant.java
@@ -0,0 +1,41 @@
+/*
+ * Indus, a toolkit to customize and adapt Java programs.
+ * Copyright (c) 2003, 2004, 2005 SAnToS Laboratory, Kansas State University
+ *
+ * This software is licensed under the KSU Open Academic License.
+ * You should have received a copy of the license with the distribution.
+ * A copy can be found at
+ *     http://www.cis.ksu.edu/santos/license.html
+ * or you can contact the lab at:
+ *     SAnToS Laboratory
+ *     234 Nichols Hall
+ *     Manhattan, KS 66506, USA
+ */
+
+package edu.ksu.cis.indus.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation captures the invariant as an OCL expression.
+ * 
+ * @author <a href="http://www.cis.ksu.edu/~rvprasad">Venkatesh Prasad Ranganath</a>
+ * @author $Author$
+ * @version $Revision$ $Date$
+ */
+@Target({ ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.TYPE }) @Documented @Retention(RetentionPolicy.RUNTIME) public @interface OCLInvariant {
+
+	/**
+	 * Provides the invariant as an OCL expression.
+	 * 
+	 * @return the invariant.
+	 */
+	String value();
+
+}
+
+// End of File
