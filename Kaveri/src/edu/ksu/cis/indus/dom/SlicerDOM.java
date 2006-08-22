ae2b09692ae34cf33e3716f5d75b101ef8919b0c
rvprasad <>
2006-08-22 23:54:27 +0000

    - some more changes.

--- Kaveri/src/edu/ksu/cis/indus/dom/SlicerDOM.java
+++ Kaveri/src/edu/ksu/cis/indus/dom/SlicerDOM.java
@@ -1,18 +1,38 @@
 package edu.ksu.cis.indus.dom;
 
 import static edu.ksu.cis.indus.kaveri.KaveriPlugin.getDefault;
+import static edu.ksu.cis.indus.kaveri.soot.SootConvertor.getLineNumber;
+import static edu.ksu.cis.indus.kaveri.soot.SootConvertor.getStmtForLine;
 import static org.eclipse.core.resources.ResourcesPlugin.getWorkspace;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.search.IJavaSearchConstants;
+import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.core.search.SearchMatch;
+import org.eclipse.jdt.core.search.SearchParticipant;
+import org.eclipse.jdt.core.search.SearchPattern;
+import org.eclipse.jdt.core.search.SearchRequestor;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
 
+import soot.SootClass;
 import soot.SootMethod;
+import soot.jimple.Stmt;
 
+import edu.ksu.cis.indus.common.datastructures.Pair;
 import edu.ksu.cis.indus.common.soot.BasicBlockGraphMgr;
 import edu.ksu.cis.indus.common.soot.IStmtGraphFactory;
 import edu.ksu.cis.indus.interfaces.ICallGraphInfo;
@@ -27,11 +47,61 @@ import edu.ksu.cis.indus.tools.slicer.SlicerTool;
 public final class SlicerDOM {
     
     public IWorkbench getWorkbench() {
-        return getWorkbench();
+        return PlatformUI.getWorkbench();
     }
     
     public IWorkspace getWorkSpace() {
         return getWorkspace();
+    }
+    
+    public ArrayList getFileFor(final SootClass sc) throws CoreException {
+        final ArrayList _result = new ArrayList();
+        final String[] _s = sc.getName().split("$");
+        final SearchEngine _se = new SearchEngine();
+        final SearchPattern _sp = SearchPattern.createPattern(_s[0], IJavaSearchConstants.TYPE, IJavaSearchConstants.DECLARATIONS, SearchPattern.R_EXACT_MATCH);
+        final SearchRequestor _r = new SearchRequestor() {
+
+            public void acceptSearchMatch(final SearchMatch match) throws CoreException {
+                _result.add((IFile) match.getResource());
+            }            
+        };
+        _se.search(_sp, new SearchParticipant[0], SearchEngine.createWorkspaceScope(), _r, new NullProgressMonitor());
+        return _result;
+    }
+    
+    public int getLineFor(final Stmt stmt) {
+        return getLineNumber(stmt);
+    }
+    
+    public boolean createMarker(final Pair expr, final String message) throws CoreException {
+        SootMethod _sootMethod = (SootMethod) expr.getSecond();
+        ArrayList _files = getFileFor(_sootMethod.getDeclaringClass());
+        assert _files.size() <= 1;
+        final boolean _ret = !_files.isEmpty();
+        if (_ret) {
+            Stmt _sootStmt = (Stmt) expr.getFirst();
+            int line = getLineFor(_sootStmt);
+            IMarker marker = ((IFile) _files.get(0)).createMarker(IMarker.BOOKMARK);
+            marker.setAttribute("Slicer", "Slicer");
+            marker.setAttribute(IMarker.MESSAGE, message);
+            marker.setAttribute(IMarker.LINE_NUMBER, line);
+        }
+        return _ret;
+    }
+    
+    public void displayDialog(final String message) {
+        MessageDialog.openInformation( getWorkbench().getActiveWorkbenchWindow().getShell(), "Kaveri Scripting Dialog", message);
+    }
+    
+    public boolean selectionExists() {
+        final IEditorPart _e = getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+        final ISelection _selection = _e.getEditorSite().getSelectionProvider().getSelection();
+        return _selection instanceof ITextSelection;
+    }
+    
+    public Collection<Stmt> getJimpleStmtsFor(final IFile thefile, final IType theclass, 
+            final IMethod themethod, final int theline) {
+        return getStmtForLine(thefile, theclass, themethod, theline);
     }
     
     public KaveriPlugin getKaveriPlugin() {
